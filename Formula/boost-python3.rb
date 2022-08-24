7df6e34700ec327df1d47388057f1bc403ab85ef
chris <chris.r.jones.1983@gmail.com>
2022-08-24 14:15:02 -0500

    boost-python3: update 1.79.0_1 bottle.

--- Formula/boost-python3.rb
+++ Formula/boost-python3.rb
@@ -12,12 +12,8 @@ class BoostPython3 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "3b00e0557668bb9a167b7aa1864db33a4b8505066288459ab05ad10745398df2"
-    sha256 cellar: :any,                 arm64_big_sur:  "646249b68231b84630fd631790ab80e9d13b7a7cb62bafa8e08fb35c926380ba"
-    sha256 cellar: :any,                 monterey:       "abd27c9a20d98a3f3acadc55225c582dcbe3457080e9f66bb480b14f549771c1"
-    sha256 cellar: :any,                 big_sur:        "0960734129fa8ddc8f3f2b7d3d35efc63d67677f99c3f1bbcb780197018ddbe8"
-    sha256 cellar: :any,                 catalina:       "20af97eca0a0c3fb287c76c2972d15a2e01f43f4bf4484c90e11045b21667b4f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8b93830c083dd4e555e5f70e5ef7ab5afaaa2feb387c2837fbd8f82d62405efa"
+    root_url "https://ghcr.io/v2/ipatch/core-mojave"
+    sha256 cellar: :any, mojave: "62ca53cb2515bfd5927012bf46a46414865ae7f73312199a733f5e75333f4f13"
   end
 
   depends_on "numpy" => :build
