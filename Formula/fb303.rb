c406d16c4c21afa28f958a2cfa40fde1714c57c2
chris <chris.r.jones.1983@gmail.com>
2023-06-06 12:35:44 -0500

    [no ci] checkin missing fb303 formula

---
+++ Formula/fb303.rb
@@ -0,0 +1,58 @@
+class Fb303 < Formula
+  desc "Thrift functions for querying information from a service"
+  homepage "https://github.com/facebook/fb303"
+  url "https://github.com/facebook/fb303/archive/refs/tags/v2023.05.22.00.tar.gz"
+  sha256 "6d4f4ae4534c2a15c05410d4925e764db9c3321fed39abc78b6a728a6c1a9aca"
+  license "Apache-2.0"
+  head "https://github.com/facebook/fb303.git", branch: "main"
+
+  depends_on "cmake" => :build
+  depends_on "fbthrift"
+  depends_on "fizz"
+  depends_on "fmt"
+  depends_on "folly"
+  depends_on "gflags"
+  depends_on "glog"
+  depends_on "openssl@1.1"
+  depends_on "wangle"
+
+  fails_with gcc: "5" # C++17
+
+  def install
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DPYTHON_EXTENSIONS=OFF",
+                    "-DBUILD_SHARED_LIBS=ON",
+                    "-DCMAKE_INSTALL_RPATH=#{rpath}",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include "fb303/thrift/gen-cpp2/BaseService.h"
+      #include <iostream>
+      int main() {
+        auto service = facebook::fb303::cpp2::BaseServiceSvIf();
+        std::cout << service.getGeneratedName() << std::endl;
+        return 0;
+      }
+    EOS
+
+    if Tab.for_formula(Formula["folly"]).built_as_bottle
+      ENV.remove_from_cflags "-march=native"
+      ENV.append_to_cflags "-march=#{Hardware.oldest_cpu}" if Hardware::CPU.intel?
+    end
+
+    ENV.append "CXXFLAGS", "-std=c++17"
+    system ENV.cxx, *ENV.cxxflags.split, "test.cpp", "-o", "test",
+                    "-I#{include}", "-I#{Formula["openssl@1.1"].opt_include}",
+                    "-L#{lib}", "-lfb303_thrift_cpp",
+                    "-L#{Formula["folly"].opt_lib}", "-lfolly",
+                    "-L#{Formula["glog"].opt_lib}", "-lglog",
+                    "-L#{Formula["fbthrift"].opt_lib}", "-lthriftprotocol", "-lthriftcpp2",
+                    "-L#{Formula["boost"].opt_lib}", "-lboost_context-mt",
+                    "-ldl"
+    assert_equal "BaseService", shell_output("./test").strip
+  end
+end
