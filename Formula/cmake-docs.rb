66105858014fe0ccd8837691da427978be73b93b
chris <chris.r.jones.1983@gmail.com>
2023-12-22 13:15:38 -0600

    cmake-docs update formula to point to 3.28.1

--- Formula/cmake-docs.rb
+++ Formula/cmake-docs.rb
@@ -1,20 +1,13 @@
 class CmakeDocs < Formula
   desc "Documentation for CMake"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.25.1/cmake-3.25.1.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/cmake-3.25.1.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.25.1.tar.gz"
-  sha256 "1c511d09516af493694ed9baf13c55947a36389674d657a2d5e0ccedc6b291d8"
+  url "https://github.com/Kitware/CMake/releases/download/v3.28.1/cmake-3.28.1.tar.gz"
+  sha256 "15e94f83e647f7d620a140a7a5da76349fc47a1bfed66d0f5cdee8e7344079ad"
   license "BSD-3-Clause"
   head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
 
   livecheck do
     formula "cmake"
-  end
-
-  bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cmake-docs"
-    sha256 cellar: :any_skip_relocation, mojave: "4b420e82e1dac0438c2a453e2e3c395cbab98e4df0ae1805e68155dbfa51b72b"
   end
 
   depends_on "cmake" => :build
