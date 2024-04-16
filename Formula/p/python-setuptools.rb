c68806eccb78ebaef2887c0ee33020031f5c571f
chris <chris.r.jones.1983@gmail.com>
2024-04-16 08:48:55 -0500

    checkin in formula from usptream, python-setuptools

---
+++ Formula/p/python-setuptools.rb
@@ -0,0 +1,27 @@
+class PythonSetuptools < Formula
+  desc "Easily download, build, install, upgrade, and uninstall Python packages"
+  homepage "https://setuptools.pypa.io/"
+  url "https://files.pythonhosted.org/packages/d6/4f/b10f707e14ef7de524fe1f8988a294fb262a29c9b5b12275c7e188864aed/setuptools-69.5.1.tar.gz"
+  sha256 "6c1fccdac05a97e598fb0ae3bbed5904ccb317337a51139dcd51453611bbb987"
+  license "MIT"
+
+  depends_on "python@3.10" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
+        .map { |f| f.opt_libexec/"bin/python" }
+  end
+
+  def install
+    pythons.each do |python|
+      system python, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      system python, "-c", "import setuptools"
+    end
+  end
+end
